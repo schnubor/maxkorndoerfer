89cecccf44cd60880c950823e11760cc0af04a78
schnubor <info@chko.org>
2019-05-26 11:44:47 +0200

    styling

--- inc/home.php
+++ inc/home.php
@@ -1,33 +1,32 @@
 <?php
-    require( TEMPLATE_DIR . 'renderProject.php' );
-    $query = new \Contentful\Delivery\Query();
-    $query->setContentType('project');
-    try {
-        $projects = $contentfulClient->getEntries($query);
-    } catch (\Contentful\Core\Exception\NotFoundException $exception) {
-        debug_to_console( 'Contentful error: ' . $exception );
-    }
+require(TEMPLATE_DIR . 'renderProject.php');
+$query = new \Contentful\Delivery\Query();
+$query->setContentType('project');
+try {
+    $projects = $contentfulClient->getEntries($query);
+} catch (\Contentful\Core\Exception\NotFoundException $exception) {
+    debug_to_console('Contentful error: ' . $exception);
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <?php renderHead( 'Max Korndörfer', 'Freischaffender Fotograf Berlin' ) ?>
+<?php renderHead('Max Korndörfer', 'Freischaffender Fotograf Berlin') ?>
 
-    <body>
-        <h1>Max Korndörfer</h1>
+<body>
+    <div class="home">
+        <h1 class="title">Max Korndörfer</h1>
 
-        <section id="projects">
-            <div class="container">
-                <ul class="navigation">
-                <?php foreach ($projects as $project) { ?>
-                    <li>
-                        <a href="/project/<?php echo $project->getId() ?>">
-                            <?php echo $project->title ?>
-                        </a>
-                    </li>    
-                <?php } ?>
-                </ul>
-            </div>
-        </section>
-    </body>
+        <ul class="navigation">
+            <?php foreach ($projects as $project) { ?>
+                <li class="navItem">
+                    <a href="/project/<?php echo $project->getId() ?>">
+                        <?php echo $project->title ?>
+                    </a>
+                </li>
+            <?php } ?>
+        </ul>
+    </div>
+</body>
+
 </html>
--- inc/project.php
+++ inc/project.php
@@ -1,40 +1,51 @@
 <?php
-    require( TEMPLATE_DIR . 'renderSlide.php' );
-    $uri = rtrim( dirname($_SERVER["SCRIPT_NAME"]), '/' );
-    $uri = '/' . trim( str_replace( $uri, '', $_SERVER['REQUEST_URI'] ), '/' );
-    $uri = urldecode( $uri );
-    $projectId = substr($uri, strrpos($uri, '/') + 1);
-    $query = new \Contentful\Delivery\Query();
-    $query->setContentType('project');
+require(TEMPLATE_DIR . 'renderSlide.php');
+$uri = rtrim(dirname($_SERVER["SCRIPT_NAME"]), '/');
+$uri = '/' . trim(str_replace($uri, '', $_SERVER['REQUEST_URI']), '/');
+$uri = urldecode($uri);
+$projectId = substr($uri, strrpos($uri, '/') + 1);
+$query = new \Contentful\Delivery\Query();
+$query->setContentType('project');
 
-    try {
-        $project = $contentfulClient->getEntry( $projectId );
-        $allProjects = $contentfulClient->getEntries($query);
-    } catch (\Contentful\Core\Exception\NotFoundException $exception) {
-        debug_to_console( 'Contentful error: ' . $exception );
-    }
+try {
+    $project = $contentfulClient->getEntry($projectId);
+    $allProjects = $contentfulClient->getEntries($query);
+} catch (\Contentful\Core\Exception\NotFoundException $exception) {
+    debug_to_console('Contentful error: ' . $exception);
+}
 
-    $renderer = new \Contentful\RichText\Renderer();
+$renderer = new \Contentful\RichText\Renderer();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<?php renderHead( $project->title, $project->seoDescription ) ?>
+<?php renderHead($project->title . ' | Max Korndoerfer', $project->seoDescription) ?>
+
 <body>
+    <div class="project">
+        <div class="row">
+            <div class="col-md-3">
+                <a href="/" class="back">&larr; back</a>
+                <h1 class="title"><?php echo $project->title ?></h1>
+                <div class="description">
+                    <?php echo nl2br($renderer->render($project->description)) ?>
+                </div>
+            </div>
 
-    <section>
-        <div class="container">
-            <h1 class="title"><?php echo $project->title ?></h2>
-
-            <?php echo nl2br($renderer->render($project->description)) ?>
-
-            <?php
-                foreach( $project->images as $asset ) {
-                    echo '<div><img src="' . $asset->getFile()->getUrl() .'" /></div>';
+            <div class="col-md-9">
+                <?php
+                foreach ($project->images as $asset) {
+                    echo '<div class="image">';
+                    echo '<img src="' . $asset->getFile()->getUrl() . '" />';
+                    if ($asset->getDescription()) {
+                        echo '<p class="caption">' . $asset->getDescription() . PHP_EOL . '</p>';
+                    }
+                    echo '</div>';
                 }
-            ?>
+                ?>
+            </div>
         </div>
-    </section>
-
+    </div>
 </body>
+
 </html>
